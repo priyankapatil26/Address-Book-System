cc64cb42067f672a68048564d974a34684a7b9c1
Priyanka Patil1 <priyanka.patil1@morningstar.com>
2021-09-12 19:33:13 +0530

    Initial commit

---
+++ AddressBookMain.java
@@ -0,0 +1,101 @@
+package AddressBook;
+	
+class AddressBookMain {
+	private  String firstName;
+	private String lastName;
+	private  String address;
+	private  String city;
+	private  String state;
+	private  int zip;
+	private  long phoneNumber;
+	private  int EmailID;
+
+	// Parameterized Constructor
+	public AddressBookMain(String firstName, String lastName, String address, String city, String state, int zip, long phoneNumber,int EmailID) {
+
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+		this.EmailID = EmailID;
+	}
+
+	public AddressBookMain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getID() {
+		return EmailID;
+	}
+
+	public void setID(int EmailID) {
+		this.EmailID = EmailID;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String toString() {
+		String addressString = "Name: " + firstName + " " + lastName + 
+				"\nPhone: " + phoneNumber + 
+				"\nAddress: " + address	+ ", " + city + ", " + state + 
+				"\nZIP: " + zip + "\nEmail Id: " + EmailID;
+		return addressString;
+}
+}
+
